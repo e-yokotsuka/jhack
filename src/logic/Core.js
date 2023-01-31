d3410af12bd7feda4eff0baec1661187c71b5b5f
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2023-01-31 17:03:43 +0900

    テキストスクロール

--- src/logic/Core.js
+++ src/logic/Core.js
@@ -1,15 +1,21 @@
-import { Application, Assets } from 'pixi.js';
+import { Application, Assets, Text } from 'pixi.js';
 
 import Stats from 'stats.js';
 import Tile from '../sprites/Tile';
+
+const CANVAS_WIDTH = 32 * 25;
+const CANVAS_HEIGHT = 32 * 15;
+// const CANVAS_WIDTH_CENTER = CANVAS_WIDTH / 2;
+// const CANVAS_HEIGHT_CENTER = CANVAS_HEIGHT / 2;
+
 
 class Core {
   constructor({ isShowStats = true }) {
     const dom = document.getElementById('contents');
     this.app = new Application({
       backgroundColor: 0x1099bb,
-      width: 32 * 25,
-      height: 32 * 15
+      width: CANVAS_WIDTH,
+      height: CANVAS_HEIGHT
     });
     if (isShowStats) {
       this.stats = new Stats();
@@ -40,9 +46,19 @@ class Core {
         t.y = y * t.height;
         app.stage.addChild(t);
       }
-
-    app.ticker.add((/*delta*/) => {
+    const text = new Text('よくぞいらした。\nここムーリダヤ・メタインでは\n恐ろしき魔物との戦いが数千年にわたって繰り広げられている。', {
+      fontSize: 24,
+      fill: 0xffffff,
+      align: 'center',
+    });
+    app.stage.addChild(text);
+    text.y = CANVAS_HEIGHT;
+    text.x = (CANVAS_WIDTH - text.width) / 2;
+    app.ticker.add((delta) => {
       stats.begin();
+      app.stage.addChild(text);
+      text.y -= delta * 0.2;
+      text.y = Math.max(text.y, 0);
       stats.end();
     });
   }
