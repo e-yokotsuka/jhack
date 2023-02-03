6022a8bcc8c240c0ae971fc1739f6a564b57315a
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2023-02-03 15:10:10 +0900

    部屋の矩形を生成

--- src/map/MP_AutoMap.js
+++ src/map/MP_AutoMap.js
@@ -1,5 +1,6 @@
 import { Container } from 'pixi.js';
 import MapSplitter from '../tools/MapSpliter';
+import RoomRectCreater from '../tools/RoomRectCreater';
 import SP_Tile from "../sprites/SP_Tile";
 class MP_AutoMap {
   constructor({ core, width = 100, height = 50 }) {
@@ -9,14 +10,18 @@ class MP_AutoMap {
     const { stage } = this.core.app;
     this.mapContainer = new Container();
     stage.addChild(this.mapContainer);
-    this.fill("blank");
     this.makeAutomap();
   }
 
   makeAutomap = _ => {
+    this.fill("blank");
     const rectArray = MapSplitter({ map: this.map, maxRoom: Math.round(Math.random() * 10 + 2) });
+    const roomArray = RoomRectCreater(rectArray);
     const tileName = ['acidic_floor_0', 'dirt_0', 'frozen_0', 'green_bones_9', 'ice_2', 'infernal_14', 'limestone_0', 'white_marble_0', 'snake-a_0', 'dirt_full', 'demonic_red_7'];
-    rectArray.forEach(({ x, y, width, height }, n) => {
+    // rectArray.forEach(({ x, y, width, height }, n) => {
+    //   this.fillRect({ x, y, width, height, cellName: `${tileName[n]}` });
+    // });
+    roomArray.forEach(({ x, y, width, height }, n) => {
       this.fillRect({ x, y, width, height, cellName: `${tileName[n]}` });
     });
     this.reset();
@@ -24,13 +29,14 @@ class MP_AutoMap {
 
   fill = (cellName) => {
     const { width, height } = this;
-    this.map = [...Array(height)].map(_ => Array(width).fill(cellName));
+    this.map = [...Array(height)].map(_ => Array(width).fill({ cellName }));
   }
 
   fillRect = ({ x, y, width, height, cellName }) => {
     for (let ny = y; ny < (y + height); ny++)
       for (let nx = x; nx < (x + width); nx++) {
-        this.map[ny][nx] = cellName;
+        // console.log(`this.map[${nx}][${ny}] ::: x:${x}/y:${y}/w:${width}/h:${height}/name:${cellName}`)
+        this.map[ny][nx] = { cellName };
       }
     this.reset();
   }
@@ -41,8 +47,8 @@ class MP_AutoMap {
     mapContainer.scale.x = 0.2;
     mapContainer.scale.y = 0.2;
     map.forEach((row, y) => {
-      row.forEach((chellName, x) => {
-        const t = SP_Tile({ core, name: chellName });
+      row.forEach(({ cellName }, x) => {
+        const t = SP_Tile({ core, name: cellName });
         t.x = x * t.width;
         t.y = y * t.height;
         mapContainer.addChild(t);
@@ -50,13 +56,13 @@ class MP_AutoMap {
     })
   }
 
-  mapText = _ => {
+  _debugMapText = _ => {
     const { map } = this;
     let rowLog = "";
     map.forEach((row) => {
       let line = "";
-      row.forEach((chellName) => {
-        line = `${line}|${chellName}`;
+      row.forEach(({ cellName }) => {
+        line = `${line}|${cellName}`;
       })
       rowLog = `${rowLog}${line}\n`;
     })
---
+++ src/tools/RoomRectCreater.js
@@ -0,0 +1,27 @@
+const RETRY_SIZE = 5;
+const MIN_SIZE = 4;
+const ROOM_SPAN = 6;
+
+const createRoomRect = (inRect, retry = 0) => {
+  if (retry > RETRY_SIZE) {
+    return [inRect];
+  }
+  const addX = Math.round(Math.random() * ROOM_SPAN) + 1;
+  const addY = Math.round(Math.random() * ROOM_SPAN) + 1;
+  const subWidth = Math.round((0.8 + Math.random() / 5) * inRect.width) - 1;
+  const subHeight = Math.round((0.8 + Math.random() / 5) * inRect.height) - 1;
+  const rect = {
+    x: inRect.x + addX,
+    y: inRect.y + addY,
+    width: subWidth - addX,
+    height: subHeight - addY,
+  }
+  if (rect.width <= MIN_SIZE || rect.height <= MIN_SIZE) return createRoomRect(inRect, retry + 1);
+  return rect;
+}
+
+const RoomRectCreater = (rectArray) => rectArray.map((rect) => createRoomRect(rect));
+
+export default RoomRectCreater;
+
+
