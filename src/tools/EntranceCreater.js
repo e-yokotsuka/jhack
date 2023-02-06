3aabaa79f15dd6ba9911d7a38e6cccd844602893
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2023-02-06 22:58:21 +0900

    変数名変更

--- src/tools/EntranceCreater.js
+++ src/tools/EntranceCreater.js
@@ -2,13 +2,13 @@
 
 const EntranceCreater = (roadArray) => {
   const entranceArray = roadArray.map(({ x, y, width, height, direction }) => {
-    const logic = {
+    const data = {
       r: _ => ({ x: x - 1, y, width: 1, height: 1, type: 'entrancce' }),
       l: _ => ({ x: x + width, y, width: 1, height: 1, type: 'entrancce' }),
       t: _ => ({ x, y: y + height, width: 1, height: 1, type: 'entrancce' }),
       b: _ => ({ x, y: y - 1, width: 1, height: 1, type: 'entrancce' }),
     }
-    return logic[direction]();
+    return data[direction]();
   });
 
 
