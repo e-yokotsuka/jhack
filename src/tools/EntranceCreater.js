4a67617cb1450ebfbb7f8aca8e349f231983684a
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2023-02-06 23:00:46 +0900

    関数を介せず、単純にデータを返すように変更

--- src/tools/EntranceCreater.js
+++ src/tools/EntranceCreater.js
@@ -3,12 +3,12 @@
 const EntranceCreater = (roadArray) => {
   const entranceArray = roadArray.map(({ x, y, width, height, direction }) => {
     const data = {
-      r: _ => ({ x: x - 1, y, width: 1, height: 1, type: 'entrancce' }),
-      l: _ => ({ x: x + width, y, width: 1, height: 1, type: 'entrancce' }),
-      t: _ => ({ x, y: y + height, width: 1, height: 1, type: 'entrancce' }),
-      b: _ => ({ x, y: y - 1, width: 1, height: 1, type: 'entrancce' }),
+      r: { x: x - 1, y, width: 1, height: 1, type: 'entrancce' },
+      l: { x: x + width, y, width: 1, height: 1, type: 'entrancce' },
+      t: { x, y: y + height, width: 1, height: 1, type: 'entrancce' },
+      b: { x, y: y - 1, width: 1, height: 1, type: 'entrancce' },
     }
-    return data[direction]();
+    return data[direction];
   });
 
 
