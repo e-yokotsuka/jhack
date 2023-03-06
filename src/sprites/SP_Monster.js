dd436b6cb0fb78d12b0c4b95214fd7a342b9d31e
e-yokotsuka <e.yokotsuka@gmail.com>
2023-03-06 22:32:19 +0900

    コメント削除

--- src/sprites/SP_Monster.js
+++ src/sprites/SP_Monster.js
@@ -33,7 +33,6 @@ class SP_Monster {
     // this.mainMap.center(this.SP_MonsterData.status.mapX, this.SP_MonsterData.status.mapY);
   }
 
-
   diceRoll = diceText => this.core.diceRoll(diceText);
 
   trappedIn = ({
@@ -57,76 +56,6 @@ class SP_Monster {
 
 
   update = (/*delta*/) => {
-    const { /*core: { input },*/ mainMap } = this;
-    this.SP_MonsterData.beforeUpdate();
-    // if (input.isSingleDown('o')) {
-    //   this.SP_MonsterData.isLock() ? this.SP_MonsterData.unlock() : this.SP_MonsterData.lock();
-    // } else if (input.isSingleDown('w') ||
-    //   input.isSingleDown('ArrowUp')) {
-    //   this.SP_MonsterData.trialMove('u');
-    // } else if (input.isSingleDown('s') ||
-    //   input.isSingleDown('ArrowDown')) {
-    //   this.SP_MonsterData.trialMove('d');
-    // } else if (input.isSingleDown('a') ||
-    //   input.isSingleDown('ArrowLeft')) {
-    //   this.SP_MonsterData.trialMove('l');
-    // } else if (input.isSingleDown('d') ||
-    //   input.isSingleDown('ArrowRight')) {
-    //   this.SP_MonsterData.trialMove('r');
-    // } else if (input.isSingleDown('.')) {
-    //   // その場にとどまる。
-    //   this.SP_MonsterData.stay();
-    // }
-    this.sprite.x = mainMap.mapContainer.x + this.SP_MonsterData.status.mapX * 32;
-    this.sprite.y = mainMap.mapContainer.y + this.SP_MonsterData.status.mapY * 32;
-    if (!this.SP_MonsterData.isMove()) return; // 動いていない
-    // 動いた
-    const { virtualX: vx, virtualY: vy } = this.SP_MonsterData.status;
-    this.SP_MonsterData.status.steps++;
-    this.core.handleStepUpdate(vx, vy);
-    const blockedTile = mainMap.isBlockedTile(vx, vy);
-    if (blockedTile) {
-      const { type = 'unknown', item, open = _ => { }, hitStep = 0, close = false } = blockedTile;
-      if (type === 'chest') {
-        if (hitStep + 1 == this.SP_MonsterData.status.steps) {
-          if (item) {
-            this.SP_MonsterData.status.items.push(item);
-            this.core.addText(`${item.itemName}をGETした！`);
-            open();
-          } else {
-            this.core.addText(`からっぽだ！`);
-          }
-        } else {
-          this.core.addText(`宝箱だ！`);
-        }
-      } else if (type === 'door') {
-        if (hitStep + 1 == this.SP_MonsterData.status.steps && close) {
-          this.core.addText(`ドアを開けた！`);
-          open();
-        } else {
-          this.core.addText(`しまった！ 閉まったドアだ！`);
-        }
-      }
-      blockedTile.hitStep = this.SP_MonsterData.status.steps;
-    } else {
-      this.SP_MonsterData.moveConfirmed(); //移動確定
-      // this.mainMap.center(vx, vy);
-      const actionTile = mainMap.isActionTile(vx, vy);
-      if (actionTile) {
-        const { type = 'unknown', trap } = actionTile;
-        if (type === 'trap') {
-          this.core.addText(`ウップス!!  ${trap.name}という、罠にハマった！`);
-          const dmg = this.trappedIn(trap);
-          if (dmg) {
-            this.mainMap.putTileWithAttributes({ x: vx, y: vy, cellName: 'dngn_trap_magical', attributes: { type: 'floor' } })
-            this.core.addText(`いてぇ。 ${dmg} ポイントのダメージをくらった！`);
-            if (this.hit(dmg)) return;//しんだ
-          } else {
-            this.core.addText(`しかし、発動前にヒョイっと避けた！`);
-          }
-        }
-      }
-    }
   }
 
 
