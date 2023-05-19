cf1914686fc27b686841129d1208c2782bde10ea
InfoNote Yokotsuka <e.yokotsuka+note@gmail.com>
2023-05-19 09:37:29 +0900

    HP、MPのプログレスバーを追加

--- src/UI/UI_Status.js
+++ src/UI/UI_Status.js
@@ -1,7 +1,9 @@
-import { Text } from 'pixi.js';
+import { Container, Text } from 'pixi.js';
+
+import UI_ProgressBar from './UI_ProgressBar';
 
 const STATUS_ABS_X = 10;
-const STATUS_RELATIVE_Y = -40;
+const STATUS_RELATIVE_Y = -60;
 
 
 class UI_Status {
@@ -10,26 +12,42 @@ class UI_Status {
     const { player, app: { screen: { height } } } = core;
     this.core = core;
     this.player = player;
-    this.prim = new Text('', {
+    this.container = new Container();
+    this.primText = new Text('', {
       fontSize: 20,
       fill: 0xffffff,
       align: 'left',
     });
-    this.prim.x = 10;
-    this.prim.y = height + STATUS_RELATIVE_Y;
+    this.progressHp = new UI_ProgressBar({
+      core,
+      y:24
+    });
+    this.progressMp = new UI_ProgressBar({
+      core,
+      fillColor:"#0f0fff",
+      backgroundColor:"#ff0f0f",
+      y:34
+    });
+    this.container.addChild(this.primText);
+    this.container.addChild(this.progressHp.getPrim());
+    this.container.addChild(this.progressMp.getPrim());
+    this.container.x = 10;
+    this.container.y = height + STATUS_RELATIVE_Y;
   }
 
-  getPrim = _ => this.prim;
+  getPrim = _ => this.container;
 
   resize = (width,height)=>{
-    this.prim.x = STATUS_ABS_X;
-    this.prim.y = height + STATUS_RELATIVE_Y;
+    this.container.x = STATUS_ABS_X;
+    this.container.y = height + STATUS_RELATIVE_Y;
   } 
 
   update = _ => {
     const { hp, maxHp, mp, maxMp, mapX, mapY, steps } = this.player.getPlayerData();
     const { isWindowOpen } = this.core;
-    this.prim.text = `HP:${hp}/${maxHp} MP: ${mp}/${maxMp} STEP ${steps} : (${mapX},${mapY}) : locked:${isWindowOpen} `;
+    this.primText.text = `HP:${hp}/${maxHp} MP: ${mp}/${maxMp} STEP ${steps} : (${mapX},${mapY}) : locked:${isWindowOpen} `;
+    this.progressHp.setValue((hp/maxHp*100));
+    this.progressMp.setValue((mp/maxMp*100));
   }
 
 }
